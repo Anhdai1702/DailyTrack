8bb546fd7a6300467ff7af25a6918ed70fbfddaf
Anhdai1702 <153721013+Anhdai1702@users.noreply.github.com>
2024-11-05 21:31:43 +0700

    DailyTrack

---
+++ DailyTrackTestApp/Resources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyTrackTestApp
+//
+//  Created by Phùng Anh Đài  on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-DailyTrackTestApp/Pods-DailyTrackTestApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyTrackTestApp : NSObject
+@end
+@implementation PodsDummy_Pods_DailyTrackTestApp
+@end
