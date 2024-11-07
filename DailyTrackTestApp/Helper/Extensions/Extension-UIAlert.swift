386779857f0e06edd1c8a0a827978e84904d06b7
Anhdai1702 <153721013+Anhdai1702@users.noreply.github.com>
2024-11-07 18:51:28 +0700

    add forgot password (#6)

--- DailyTrackTestApp/Helper/Extensions/Extension-UIAlert.swift
+++ DailyTrackTestApp/Helper/Extensions/Extension-UIAlert.swift
@@ -2,19 +2,22 @@
 import UIKit
 
 extension UIViewController {
-    func hiddenBack() {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-}
-
-extension UIViewController {
+    
     func showAlert(title: String, mess: String) {
         let alert = UIAlertController(title: title, message: mess, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
     }
-}
-
-
     
+    func showAlertWithCancel(title: String, mess: String, onCancel: @escaping () -> Void) {
+        let alert = UIAlertController(title: title, message: mess, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        let cancelAction = UIAlertAction(title: "Back to login", style: .cancel) { _ in
+            onCancel()
+        }
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
