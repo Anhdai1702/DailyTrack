257bac2289a0829450df53eae1e6b14dfde94900
Anhdai1702 <153721013+Anhdai1702@users.noreply.github.com>
2024-11-07 08:11:36 +0700

    add login

---
+++ DailyTrackTestApp/Helper/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  DailyTrackTestApp
+//
+//  Created by Phùng Anh Đài  on 6/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    public static let color1640D6: UIColor = UIColor(hex: "#1640D6")
+    public static let color8A8E9E: UIColor = UIColor(hex: "#8A8E9E")
+    public static let color355BE1: UIColor = UIColor(hex: "#355BE1")
+    public static let color1E1F22: UIColor = UIColor(hex: "#1E1F22")
+    public static let colorE5484D: UIColor = UIColor(hex: "#E5484D")
+    public static let colorED0B4F: UIColor = UIColor(hex: "#ED0B4F")
+    public static let color0F2C94: UIColor = UIColor(hex: "#0F2C94")
+    public static let color25252E: UIColor = UIColor(hex: "#25252E")
+    public static let colorED5AB3: UIColor = UIColor(hex: "#ED5AB3")
+    public static let color83CC85: UIColor = UIColor(hex: "#83CC85")
+}
